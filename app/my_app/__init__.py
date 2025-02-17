9f1d4d978b7321109796f66cf72a8137f1758ec1
devops4sure <novjuanp@gmail.com>
2025-02-17 14:18:51 -0600

    Templates con Jinja

--- app/my_app/__init__.py
+++ app/my_app/__init__.py
@@ -4,7 +4,7 @@ from my_app.tasks.controllers import taskRoute  # Importa la ruta de los modulos
 
 from my_app.config import DevConfig
 
-app = Flask(__name__)
+app = Flask(__name__) # , template_folder="/templates" se puede poner junto con __name__ para nombrar a la carpeta de html's
 #app.config.from_object(DevConfig)  # Esto es lo mismo que decir "app.debug = True"
 
 app.register_blueprint(taskRoute)  # Registra los modulos
--- app/my_app/tasks/controllers.py
+++ app/my_app/tasks/controllers.py
@@ -1,13 +1,26 @@
 # Los controllers o views en otros frameworks son donde se alojan las rutas
-from flask import Blueprint
+from flask import Blueprint, render_template,  request, redirect, url_for
 
 # Esto es una variable de ruteo, para definir las rutas, el primero es el nombre, el segundo el metodo, el tercero el url de la ruta
 taskRoute = Blueprint("index", __name__, url_prefix="/",)  # "/" es la ruta inicial
 
 
-@taskRoute.route("/")  # "/" es la ruta del index pero dentro de "/tasks" osea /tasks/
+task_list = ["task1", "task2", "task3"]
+
+@taskRoute.route("/")
 def index():
-    return "Index"
+    return render_template("dashboard/task/index.html", tasks=task_list)
+
+
+@taskRoute.route("/practica")  # "/" es la ruta del index pero dentro de "/tasks" osea /tasks/
+def practica():
+    name = request.args.get("name", "Desarrollolibre")  # "request" es un atributo de  Flask.
+    # "Desarrollolibre" es el mensaje que esta obteniendo render_template y se envia a index.html
+    # Si en el url agregamos "/?name=juan" entonces se cambiara el mensaje a "juan"
+    # "name" dentro del "get" es la palabra clave para cambiar el mensaje, si fuera "nombre": "/?nombre=juan" seria el comando
+    hobbies = ["Videogames", "Puzzles", "Boardgames", "Anime", "Bowling"]
+    task = "Pablo no entrego la tarea!"
+    return render_template("practica.html", name=name, task=task, hobbies=list(enumerate(hobbies)))
 
 @taskRoute.route("/<int:id>")
 def show(id:int):
@@ -15,12 +28,24 @@ def show(id:int):
 
 @taskRoute.route("/delete/<int:id>")
 def delete(id:int):
-    return f"Delete {id}"
+    del task_list[id]
+    return redirect(url_for("index.index"))
 
 @taskRoute.route("/create", methods=("GET", "POST"))
 def create():
-    return "create"
+    task = request.form.get("task")  # Este dato lo obtiene del formulario en "create.html" el "input" tiene el nombre "task"
+    if task:
+        task_list.append(task)  # Agrega a la lista todo lo que se ponga en el input del formulario y se proyecta en route("/")
+        print(task_list)
+        #return redirect("/")  # redirect es un metodo de Flask que te ayuda a redireccionar a otra ruta
+        return redirect(url_for("index.index"))  # funciona igual que el anterior pero escribiendo la ruta
+        # El primer "index" es el nombre de la ruta origen, el segundo es la ruta index, podria ser practica u otra
+    return render_template("dashboard/task/create.html")
 
-@taskRoute.route("/update/<int:id>", methods=("GET", "PUT"))
+@taskRoute.route("/update/<int:id>", methods=("GET", "POST"))
 def update(id):
-    return f"Update {id}"
+    task = request.form.get("task")
+    if task:
+        task_list[id] = task  # modifica el valor de la lista que tenga el indice de "id"
+        return redirect(url_for("index.index"))
+    return render_template("dashboard/task/update.html")
